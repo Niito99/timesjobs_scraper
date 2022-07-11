5aaa02e43e0c4d32756751365afe890758caceed
Niito99 <okinetheblessed@gmail.com>
2022-07-11 17:00:17 +0000

    This commit was for the functionality that allows the code to accept more than one unfamiliar skill

--- main.py
+++ main.py
@@ -8,8 +8,11 @@ def scrape():
     search_loc = input("What location are basing your search on? \n If you want it to be worldwide input a space \n")
     search_exp = input("What is the minimum work experience that favors you \n If you want an entry level job, "
                        "input 0 \n")
-    search_unfamiliar_skill = input("What skill are you unfamiliar with\n")
+    search_unfamiliar_skill = input("What skill are you unfamiliar with\n If more than one separate with a comma \n "
+                                    "If you input more than 3,only the first 3 will be used \n")
     print("filtering out " + search_unfamiliar_skill + " ...")
+    search_unfamiliar_skill = search_unfamiliar_skill.split(",")
+    length = len(search_unfamiliar_skill)
 
     skill = f"{search_skill}"
     loc = f"{search_loc}"
@@ -24,13 +27,18 @@ def scrape():
         company_name = job.h3.text.strip()
         more_details = job.h2.a["href"]
         time_stamp = job.find("span", class_="sim-posted").text.strip()
-        skills = job.find("span", class_ = "srp-skills").text.strip()
+        skills = job.find("span", class_="srp-skills").text.strip()
         if "few" in time_stamp:
             continue
-        if search_unfamiliar_skill in skills:
+        if search_unfamiliar_skill[length-length] in skills:
+            continue
+        if search_unfamiliar_skill[length-(length-1)] in skills:
+            continue
+        if search_unfamiliar_skill[length-(length-2)] in skills:
             continue
 
-        print(f"Company Name: {company_name} \nJob Name: {job_name}\nRequired Skills: {skills}\nMore Details: {more_details}")
+        print(f"Company Name: {company_name} \nJob Name: {job_name}\nRequired Skills: {skills}\nMore Details: "
+              f"{more_details}")
         print("(" + time_stamp + ")")
         print("\n")
 
